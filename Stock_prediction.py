4d17f220d0bbcbc75bf355e4c4f3d0c186c62e5d
Shubham Jain <48795121+Shubham11Jain@users.noreply.github.com>
2020-06-17 08:01:19 +0530

    Added same code with .py extension for python users.

---
+++ Stock_prediction.py
@@ -0,0 +1,88 @@
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# Some of the needful packages to import are as follows:
+
+import numpy as np # for statistical analysis and linear algebra
+import pandas as pd # for data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt #for generating graph based solution/prediction
+from matplotlib import style
+import datetime as dt1  #for real time and date
+from datetime import datetime as dt
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+# kindlu use the path of that folder where are the files are installed.
+#here i am using my own local path
+
+from subprocess import check_output
+print(check_output(["dir",r"C:\Users\Shubham Jain\Downloads\AsknBid"],shell=True).decode("utf8"))
+
+# Any results you write to the current directory are saved as output.
+
+
+
+df=pd.read_csv('../AsknBid/prices.csv')
+df.tail()
+df=df.loc[df['symbol'] == 'CSCO']
+print(df.tail())
+
+
+
+from sklearn import preprocessing, svm
+from sklearn.model_selection import cross_validate
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import math
+
+
+forecast_col = 'close'
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
+print(forecast_out)
+df['label'] = df[forecast_col].shift(-forecast_out)
+print(df.head())
+
+
+
+#X = np.array(df.drop(['label'], 1))
+
+X=np.array(df.drop(['label','symbol','date'], axis=1))
+#print(X)
+X = preprocessing.scale(X)
+#print(X)
+X_lately = X[-forecast_out:]
+#print(X_lately)
+X = X[:-forecast_out]
+df.dropna(inplace=True)
+y = np.array(df['label'])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
+print(confidence)
+
+
+forecast_set = clf.predict(X_lately)
+print(forecast_set)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].date
+print(last_date)
+last_date=dt.strptime(last_date, '%Y-%m-%d').timestamp()
+
+last_unix = last_date
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = dt.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+print(df.tail())
+df['close'].plot()
+df['Forecast'].plot()
+#plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
